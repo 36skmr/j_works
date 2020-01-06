0b2f28f50efe0f37685882105057fe91e825aae6
36skmr <floz_30cc@yahoo.co.jp>
2020-01-06 22:03:52 +0900

    'ループで取得'

--- personal-crawl.py
+++ personal-crawl.py
@@ -1,24 +1,37 @@
 import requests
+import pprint
 from bs4 import BeautifulSoup
 
-# なぜかじーこ
-island_url = "https://j-island.net/artist/person/id/13"
+# プロフィールはID以降の数字が変わる
+island_url = "https://j-island.net/artist/person/id/"
 
-response = requests.get(island_url)
-response.encoding = response.apparent_encoding
+personal = {}
 
-bs = BeautifulSoup(response.text, 'html.parser')
+# プロフィール分だけfor文でまわす、いったんべた書き
+for num in range(1,10):
+    url = island_url + str(num)
 
-# tag
-div_tag_list = bs.select('div.artist-profile-area')
+    response = requests.get(url)
+    response.encoding = response.apparent_encoding
 
-# 趣味を取りだす
-personal = []
-person = {}
-dl_list = div_tag_list[0].select('dl')
-#print(dl_list[3])
+    bs = BeautifulSoup(response.text, 'html.parser')
 
-dd = dl_list[3].find('dd')
-print(dd.text)
-syumi_list = dd.text.split('、')
-print(syumi_list)
+    artist_name = bs.select('span.artist-name')
+
+    # tag
+    div_tag_list = bs.select('div.artist-profile-area')
+
+    if len(div_tag_list) > 0:
+
+        # 名前
+        name = artist_name[0].text
+        # 趣味を取りだす
+        dl_list = div_tag_list[0].select('dl')
+
+        dd = dl_list[3].find('dd')
+        # 全員が全員「。」で区切っていないため、半角スペースを「、」で置き換え
+        dd = dd.text.replace(' ', '、')
+        syumi_list = dd.split('、')
+        personal[name] = syumi_list
+
+pprint.pprint(personal)
